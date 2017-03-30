9f96c2a83718d04571b55ed28e782470ceb52b2e
vgelani <vishalgelani99@gmail.com>
2017-03-30 08:53:36 +0530

    Create grid for post data

---
+++ app/code/Vishal/Designer/Controller/Adminhtml/Post/Index.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Vishal_Designer extension
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @category  Sample
+ * @package   Vishal_Designer
+ * @copyright 2016 Vishal Gelani
+ * @license   http://opensource.org/licenses/mit-license.php MIT License
+ * @author    Vishal Gelani
+ */
+namespace Vishal\Designer\Controller\Adminhtml\Post;
+
+/**
+ * Designer post list controller
+ */
+class Index extends \Magento\Backend\App\Action
+{
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Vishal_Designer::post');
+        $resultPage->addBreadcrumb(__('Vishal'), __('Vishal'));
+        $resultPage->addBreadcrumb(__('Designers'), __('Designers'));
+        $resultPage->getConfig()->getTitle()->prepend(__('Designers'));
+        return $resultPage;
+    }
+}
