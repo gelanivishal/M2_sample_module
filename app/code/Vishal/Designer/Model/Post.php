53f215afe86329195cb499cbcc1d6ca98737a15e
vgelani <vishalgelani99@gmail.com>
2017-03-31 23:37:36 +0530

    Create image field for form

--- app/code/Vishal/Designer/Model/Post.php
+++ app/code/Vishal/Designer/Model/Post.php
@@ -20,6 +20,16 @@ class Post extends \Magento\Framework\Model\AbstractModel implements \Vishal\Des
 {
     const CACHE_TAG = 'vishal_designer_post';
 
+    /**
+     * Gallery images separator
+     */
+    const GALLERY_IMAGES_SEPARATOR = ';';
+
+    /**
+     * Base media folder path
+     */
+    const BASE_MEDIA_PATH = 'designer';
+
     protected function _construct()
     {
         $this->_init('Vishal\Designer\Model\ResourceModel\Post');
@@ -30,4 +40,80 @@ class Post extends \Magento\Framework\Model\AbstractModel implements \Vishal\Des
         return [self::CACHE_TAG . '_' . $this->getId()];
     }
 
+    /**
+     * Initialize dependencies.
+     *
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Cms\Model\Template\FilterProvider $filterProvider
+     * @param \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb $resourceCollection
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Store\Model\StoreManagerInterface $storeManagerInterface,
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Cms\Model\Template\FilterProvider $filterProvider,
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        parent::__construct($context, $registry, $resource, $resourceCollection, $data);
+        $this->_storeManager = $storeManagerInterface;
+        $this->filterProvider = $filterProvider;
+        $this->_productCollectionFactory = $productCollectionFactory;
+        $this->_relatedPostsCollection = clone($this->getCollection());
+    }
+
+    /**
+     * Retrieve model title
+     * @param  boolean $plural
+     * @return string
+     */
+    public function getOwnTitle($plural = false)
+    {
+        return $plural ? 'Posts' : 'Post';
+    }
+
+    /**
+     * Retrieve featured image url
+     * @return string
+     */
+    public function getFeaturedImage()
+    {
+        if (!$this->hasData('featured_image')) {
+            if ($file = $this->getData('featured_img')) {
+                $path = $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
+                $image = $path.$file;
+            } else {
+                $image = false;
+            }
+            $this->setData('featured_image', $image);
+        }
+
+        return $this->getData('featured_image');
+    }
+
+    /**
+     * Retrieve og image url
+     * @return string
+     */
+    public function getOgImage()
+    {
+        if (!$this->hasData('og_image')) {
+
+            if ($file = $this->getData('og_img')) {
+                $path = $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
+                $image = $path.$file;
+            } else {
+                $image = false;
+            }
+            $this->setData('og_image', $image);
+        }
+
+        return $this->getData('og_image');
+    }
 }
--- app/code/Vishal/Designer/Model/ResourceModel/Post.php
+++ app/code/Vishal/Designer/Model/ResourceModel/Post.php
@@ -18,8 +18,65 @@
 namespace Vishal\Designer\Model\ResourceModel;
 class Post extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
 {
+    /**
+     * @var \Magento\Framework\Stdlib\DateTime\DateTime
+     */
+    protected $_date;
+
+    /**
+     * @var \Magento\Framework\Stdlib\DateTime
+     */
+    protected $dateTime;
+
+    /**
+     * Construct
+     *
+     * @param \Magento\Framework\Model\ResourceModel\Db\Context $context
+     * @param \Magento\Framework\Stdlib\DateTime $dateTime
+     * @param string|null $resourcePrefix
+     */
+    public function __construct(
+        \Magento\Framework\Model\ResourceModel\Db\Context $context,
+        \Magento\Framework\Stdlib\DateTime\DateTime $date,
+        \Magento\Framework\Stdlib\DateTime $dateTime,
+        $resourcePrefix = null
+    ) {
+        parent::__construct($context, $resourcePrefix);
+        $this->_date = $date;
+        $this->dateTime = $dateTime;
+    }
+
     protected function _construct()
     {
         $this->_init('vishal_designer_post','post_id');
+    }
+
+    /**
+     * Process post data before saving
+     *
+     * @param \Magento\Framework\Model\AbstractModel $object
+     * @return $this
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    protected function _beforeSave(\Magento\Framework\Model\AbstractModel $object)
+    {
+        foreach (['publish_time', 'custom_theme_from', 'custom_theme_to'] as $field) {
+            $value = $object->getData($field) ?: null;
+            $object->setData($field, $this->dateTime->formatDate($value));
+        }
+
+        $gmtDate = $this->_date->gmtDate();
+
+        if ($object->isObjectNew() && !$object->getCreationTime()) {
+            $object->setCreationTime($gmtDate);
+        }
+
+        if (!$object->getPublishTime()) {
+            $object->setPublishTime($object->getCreationTime());
+        }
+
+        $object->setUpdateTime($gmtDate);
+
+        return parent::_beforeSave($object);
     }
 }
