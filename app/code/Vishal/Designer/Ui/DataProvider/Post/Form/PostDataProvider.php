94707a95cb39a59591e04667f753fa3434267afd
vgelani <vishalgelani99@gmail.com>
2017-03-31 23:13:56 +0530

    Create form of post data #2

---
+++ app/code/Vishal/Designer/Ui/DataProvider/Post/Form/PostDataProvider.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Vishal_Designer extension
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @category  Sample
+ * @package   Vishal_Designer
+ * @copyright 2016 Vishal Gelani
+ * @license   http://opensource.org/licenses/mit-license.php MIT License
+ * @author    Vishal Gelani
+ */
+namespace Vishal\Designer\Ui\DataProvider\Post\Form;
+
+use Vishal\Designer\Model\ResourceModel\Post\CollectionFactory;
+use Magento\Framework\App\Request\DataPersistorInterface;
+
+/**
+ * Class DataProvider
+ */
+class PostDataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
+{
+    /**
+     * @var \Vishal\Designer\Model\ResourceModel\Post\Collection
+     */
+    protected $collection;
+
+    /**
+     * @var DataPersistorInterface
+     */
+    protected $dataPersistor;
+
+    /**
+     * @var array
+     */
+    protected $loadedData;
+
+    /**
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param CollectionFactory $postCollectionFactory
+     * @param DataPersistorInterface $dataPersistor
+     * @param array $meta
+     * @param array $data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $postCollectionFactory,
+        DataPersistorInterface $dataPersistor,
+        array $meta = [],
+        array $data = []
+    ) {
+        $this->collection = $postCollectionFactory->create();
+        $this->dataPersistor = $dataPersistor;
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+        $this->meta = $this->prepareMeta($this->meta);
+    }
+
+    /**
+     * Prepares Meta
+     *
+     * @param array $meta
+     * @return array
+     */
+    public function prepareMeta(array $meta)
+    {
+        return $meta;
+    }
+
+    /**
+     * Get data
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        if (isset($this->loadedData)) {
+            return $this->loadedData;
+        }
+        $items = $this->collection->getItems();
+        /** @var $post \Vishal\Designer\Model\Post */
+        foreach ($items as $post) {
+            $post = $post->load($post->getId()); //temporary fix
+            $data = $post->getData();
+
+            /* Prepare Featured Image */
+            $map = [
+                'featured_img' => 'getFeaturedImage',
+                'og_img' => 'getOgImage'
+            ];
+            foreach ($map as $key => $method) {
+                if (isset($data[$key])) {
+                    $name = $data[$key];
+                    unset($data[$key]);
+                    $data[$key][0] = [
+                        'name' => $name,
+                        'url' => $post->$method(),
+                    ];
+                }
+            }
+
+//            $data['data'] = ['links' => []];
+//
+//            /* Prepare related posts */
+//            $collection = $post->getRelatedPosts();
+//            $items = [];
+//            foreach($collection as $item) {
+//                $items[] = [
+//                    'id' => $item->getId(),
+//                    'title' => $item->getTitle(),
+//                ];
+//            }
+//            $data['data']['links']['post'] = $items;
+//
+//            /* Prepare related products */
+//            $collection = $post->getRelatedProducts()->addAttributeToSelect('name');
+//            $items = [];
+//            foreach($collection as $item) {
+//                $items[] = [
+//                    'id' => $item->getId(),
+//                    'name' => $item->getName(),
+//                ];
+//            }
+//            $data['data']['links']['product'] = $items;
+
+            /* Set data */
+            $this->loadedData[$post->getId()] = $data;
+        }
+
+        return $this->loadedData;
+    }
+}
