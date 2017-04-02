b1c4d985c9ca13c8c1f3b19552d9d4433e253faf
vgelani <vishalgelani99@gmail.com>
2017-04-02 21:40:32 +0530

    Create join query for collection or add store filter

--- app/code/Vishal/Designer/Model/ResourceModel/Post.php
+++ app/code/Vishal/Designer/Model/ResourceModel/Post.php
@@ -79,4 +79,99 @@ class Post extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
 
         return parent::_beforeSave($object);
     }
+
+    /**
+     * Assign post to store views, categories, related posts, etc.
+     *
+     * @param \Magento\Framework\Model\AbstractModel $object
+     * @return $this
+     */
+    protected function _afterSave(\Magento\Framework\Model\AbstractModel $object)
+    {
+        $oldIds = $this->lookupStoreIds($object->getId());
+        $newIds = (array)$object->getStoreIds();
+        if (!$newIds) {
+            $newIds = [0];
+        }
+
+        $this->_updateLinks($object, $newIds, $oldIds, 'vishal_designer_post_store', 'store_id');
+
+        return parent::_afterSave($object);
+    }
+
+    /**
+     * Get store ids to which specified item is assigned
+     *
+     * @param int $postId
+     * @return array
+     */
+    public function lookupStoreIds($postId)
+    {
+        return $this->_lookupIds($postId, 'vishal_designer_post_store', 'store_id');
+    }
+
+    /**
+     * Get ids to which specified item is assigned
+     * @param  int $postId
+     * @param  string $tableName
+     * @param  string $field
+     * @return array
+     */
+    protected function _lookupIds($postId, $tableName, $field)
+    {
+        $adapter = $this->getConnection();
+
+        $select = $adapter->select()->from(
+            $this->getTable($tableName),
+            $field
+        )->where(
+            'post_id = ?',
+            (int)$postId
+        );
+
+        return $adapter->fetchCol($select);
+    }
+
+    /**
+     * Update post connections
+     * @param  \Magento\Framework\Model\AbstractModel $object
+     * @param  Array $newRelatedIds
+     * @param  Array $oldRelatedIds
+     * @param  String $tableName
+     * @param  String  $field
+     * @param  Array  $rowData
+     * @return void
+     */
+    protected function _updateLinks(
+        \Magento\Framework\Model\AbstractModel $object,
+        Array $newRelatedIds,
+        Array $oldRelatedIds,
+        $tableName,
+        $field,
+        $rowData = []
+    ) {
+        $table = $this->getTable($tableName);
+
+        $insert = $newRelatedIds;
+        $delete = $oldRelatedIds;
+
+        if ($delete) {
+            $where = ['post_id = ?' => (int)$object->getId(), $field.' IN (?)' => $delete];
+
+            $this->getConnection()->delete($table, $where);
+        }
+
+        if ($insert) {
+            $data = [];
+
+            foreach ($insert as $id) {
+                $id = (int)$id;
+                $data[] = array_merge(['post_id' => (int)$object->getId(), $field => $id],
+                    (isset($rowData[$id]) && is_array($rowData[$id])) ? $rowData[$id] : []
+                );
+            }
+
+            $this->getConnection()->insertMultiple($table, $data);
+        }
+    }
 }
